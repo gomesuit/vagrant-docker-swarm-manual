5b816b45fd1a8f62d7df696a7cde0af2c1b49eb2
gomesuit <gomesuit@gmail.com>
2016-06-16 13:01:11 +0900

    first commit

---
+++ set-hosts.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4" > /etc/hosts
+echo "::1         localhost localhost.localdomain localhost6 localhost6.localdomain6" >> /etc/hosts
+echo "192.168.33.10 master" >> /etc/hosts
+echo "192.168.33.20 node01" >> /etc/hosts
+echo "192.168.33.30 node02" >> /etc/hosts
